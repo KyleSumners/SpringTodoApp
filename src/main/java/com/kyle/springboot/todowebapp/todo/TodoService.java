2d9a009dc70c32937f4c5bb77d522bcf56e5e63d
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-05-20 13:13:38 -0500

    Add basic todo view

--- src/main/java/com/kyle/springboot/todowebapp/todo/TodoService.java
+++ src/main/java/com/kyle/springboot/todowebapp/todo/TodoService.java
@@ -1,17 +1,21 @@
 package com.kyle.springboot.todowebapp.todo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
-	private static List<Todo> todos;
+	private static List<Todo> todos = new ArrayList();
 	
 	static {
-		todos.add(new Todo(1, "in28minutes", "Learn AWS", 
+		todos.add(new Todo(1, "Kyle", "Learn AWS", 
 				            LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(2, "in28minutes", "Learn Spring", 
+		todos.add(new Todo(2, "Kyle", "Learn Spring", 
 	            LocalDate.now().plusYears(2), false));
-		todos.add(new Todo(3, "in28minutes", "Learn DevOps", 
+		todos.add(new Todo(3, "Kyle", "Learn DevOps", 
 	            LocalDate.now().plusYears(3), false));
 	}
 	
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>List Todos Page</title>
+    </head>
+    <body>
+        Welcome to your todos!
+        <div>${todos}</div>
+    </body>
+</html>
